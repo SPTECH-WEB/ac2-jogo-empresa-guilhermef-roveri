908e4edda19f09417d3c7696cac4e539ab54478a
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-10-22 14:00:24 -0300

    Prova finalizada

--- src/main/java/school/sptech/Empresa.java
+++ src/main/java/school/sptech/Empresa.java
@@ -1,5 +1,120 @@
 package school.sptech;
 
-public class Empresa {
+import com.sun.jdi.connect.Connector;
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.JogoInvalidoException;
+import school.sptech.exception.JogoNaoEncontradoException;
 
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Empresa {
+    private String nome;
+    private List<Jogo> jogos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Jogo> getJogos() {
+        return jogos;
+    }
+
+    public Empresa(){
+        this.nome = nome;
+        this.jogos = jogos;
+    }
+
+    public void adicionarJogo(Jogo jogo) {
+        if (jogo == null) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getCodigo() == null || jogo.getCodigo().isEmpty()) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getNome() == null || jogo.getNome().isEmpty()) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getGenero() == null || jogo.getGenero().isEmpty()) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getPreco() == null || jogo.getPreco() <= 0) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getAvaliacao() > 5 || jogo.getAvaliacao() < 0) {
+            throw new JogoInvalidoException();
+        } else if (jogo.getDataLancamento() == null || jogo.getDataLancamento().isAfter(LocalDate.now())) {
+            throw new JogoInvalidoException();
+        } else {
+            jogos.add(jogo);
+        }
+    }
+
+    public Jogo buscarJogoPorCodigo(String codigo) {
+        boolean jogoEncontrado = false;
+        if (codigo == null || codigo.isEmpty() || codigo == "" || codigo.isBlank()) {
+            throw new ArgumentoInvalidoException("Código inválido");
+        }
+        for (int i = 0; i < jogos.size(); i++) {
+            if (jogos.get(i).getCodigo() == codigo) {
+                jogoEncontrado = true;
+                return jogos.get(i);
+            }
+        }
+        if(jogoEncontrado == false){
+            throw new JogoNaoEncontradoException();
+        }
+        return null;
+    }
+
+    public void removerJogoPorCodigo(String codigo){
+        boolean jogoEncontrado = false;
+        if (codigo == null || codigo.isEmpty() || codigo == "" || codigo.isBlank()) {
+            throw new ArgumentoInvalidoException();
+        }
+        for (int i = 0; i < jogos.size(); i++) {
+            if (jogos.get(i).getCodigo() == codigo) {
+                jogos.remove(jogos.get(i));
+                jogoEncontrado = true;
+            }
+        }
+            if(!jogoEncontrado){
+                throw new JogoNaoEncontradoException();
+            }
+
+    }
+
+    public Jogo buscarJogoComMelhorAvaliacao(){
+        if(jogos.isEmpty()){
+            throw new JogoNaoEncontradoException();
+        }
+        int ultimoJogo = 0;
+        for(int i = 0; i < jogos.size(); i++){
+            if(jogos.get(i).getAvaliacao() > jogos.get(ultimoJogo).getAvaliacao()){
+                ultimoJogo = i;
+            }else if(jogos.get(i).getAvaliacao() == jogos.get(ultimoJogo).getAvaliacao()){
+                if(jogos.get(i).getDataLancamento().isAfter(jogos.get(ultimoJogo).getDataLancamento())){
+                    ultimoJogo = i;
+                }
+            }
+        }
+        return jogos.get(ultimoJogo);
+    }
+
+    public List<Jogo> buscarJogoPorPeriodo(LocalDate dataInicio, LocalDate dataFim){
+        if(dataInicio == null || dataFim == null || dataInicio.isBefore(dataFim)){
+            throw new ArgumentoInvalidoException();
+        }
+        List<Jogo> jogosEncontrados = null;
+        for(int i = 0; i < jogos.size(); i++){
+            if(jogos.get(i).getDataLancamento().equals(dataFim) || jogos.get(i).equals(dataInicio)){
+                jogosEncontrados.add(jogos.get(i));
+            }
+        }
+        return jogosEncontrados;
+    }
 }
+
+
--- src/main/java/school/sptech/Jogo.java
+++ src/main/java/school/sptech/Jogo.java
@@ -1,5 +1,78 @@
 package school.sptech;
 
+import java.time.LocalDate;
+
 public class Jogo {
+    private String codigo;
+    private String nome;
+    private String genero;
+    private Double preco;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Jogo(){};
+
+    public String getAvaliacaoDescricao(){
+
+        if(this.getAvaliacao() >= 4.5){
+            return "EXCELENTE";
+        }else if(this.getAvaliacao() >= 3.5){
+            return "BOM";
+        }else if(this.getAvaliacao() >= 2.5){
+            return "REGULAR";
+        }else{
+            return "RUIM";
+        }
+
+    }
+
 
 }
